1a8056811a4d3be0e738ab882143fc826fee54d3
Dana Gabriela <dagamendoza12@gmail.com>
2021-03-14 19:46:39 -0500

    se creo la conexion a la base de datos

--- appE4BloodBank/Datos/clConexion.cs
+++ appE4BloodBank/Datos/clConexion.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
-namespace appE4BloodBank.Datos
+
+namespace appE4BoodBank.Datos
 {
     class clConexion
     {
+
+        MySqlConnection objConexion = null;
+
+
+        public clConexion()
+        {
+            try
+            {
+                objConexion = new MySqlConnection("server=localhost; user id=root;Port=3306;database=bdBanco;Password=1002727239");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+        }
+
+
+
+        public DataTable mtdDesconectado(string consulta)
+        {
+            objConexion.Open();
+            MySqlDataAdapter adaptador = new MySqlDataAdapter(consulta, objConexion);
+            DataTable tblDatos = new DataTable();
+            adaptador.Fill(tblDatos);
+            objConexion.Close();
+            return tblDatos;
+        }
+
+        public int mtdConectado(string consulta)
+        {
+            objConexion.Open();
+            MySqlCommand comando = new MySqlCommand(consulta, objConexion);
+            int filasAfectadas = comando.ExecuteNonQuery();
+            objConexion.Close();
+            return filasAfectadas;
+        }
+
+
+
     }
 }
---
+++ appE4BloodBank/Presentacion/frmInformacionBanco.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appE4BloodBank.Presentacion
+{
+    public partial class frmInformacionBanco : Form
+    {
+        public frmInformacionBanco()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
