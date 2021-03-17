443d4614ec26e77b01fe03c450cdc693ad85ca4f
Dana Gabriela <dagamendoza12@gmail.com>
2021-03-16 21:49:45 -0500

    se creo el login

---
+++ appE4BloodBank/Presentacion/frmLogin.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace appE4BloodBank.Presentacion
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.btnINICIAR = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbUsuario = new System.Windows.Forms.TextBox();
+            this.tbContraseña = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Firebrick;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel1.Size = new System.Drawing.Size(250, 330);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtuser
+            // 
+            this.txtuser.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtuser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtuser.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtuser.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtuser.Location = new System.Drawing.Point(314, 52);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(113, 34);
+            this.txtuser.TabIndex = 1;
+            this.txtuser.Text = " Usuario";
+            // 
+            // txtpass
+            // 
+            this.txtpass.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtpass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtpass.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpass.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtpass.Location = new System.Drawing.Point(314, 113);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(142, 34);
+            this.txtpass.TabIndex = 2;
+            this.txtpass.Text = " Contraseña";
+            // 
+            // btnINICIAR
+            // 
+            this.btnINICIAR.BackColor = System.Drawing.Color.DimGray;
+            this.btnINICIAR.FlatAppearance.BorderSize = 0;
+            this.btnINICIAR.ForeColor = System.Drawing.Color.Black;
+            this.btnINICIAR.Location = new System.Drawing.Point(314, 207);
+            this.btnINICIAR.Name = "btnINICIAR";
+            this.btnINICIAR.Size = new System.Drawing.Size(417, 47);
+            this.btnINICIAR.TabIndex = 3;
+            this.btnINICIAR.Text = "INCIAR";
+            this.btnINICIAR.UseVisualStyleBackColor = false;
+            this.btnINICIAR.Click += new System.EventHandler(this.btnINICIAR_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.linkLabel1.DisabledLinkColor = System.Drawing.Color.Red;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Maroon;
+            this.linkLabel1.Location = new System.Drawing.Point(464, 272);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(119, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Olvideste tu contraseña";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(502, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(502, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // tbUsuario
+            // 
+            this.tbUsuario.Location = new System.Drawing.Point(483, 62);
+            this.tbUsuario.Name = "tbUsuario";
+            this.tbUsuario.Size = new System.Drawing.Size(211, 20);
+            this.tbUsuario.TabIndex = 7;
+            // 
+            // tbContraseña
+            // 
+            this.tbContraseña.Location = new System.Drawing.Point(483, 119);
+            this.tbContraseña.Name = "tbContraseña";
+            this.tbContraseña.PasswordChar = '*';
+            this.tbContraseña.Size = new System.Drawing.Size(211, 20);
+            this.tbContraseña.TabIndex = 8;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(780, 330);
+            this.Controls.Add(this.tbContraseña);
+            this.Controls.Add(this.tbUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btnINICIAR);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmLogin";
+            this.Opacity = 0.8D;
+            this.Text = "frmLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Button btnINICIAR;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbUsuario;
+        private System.Windows.Forms.TextBox tbContraseña;
+    }
+}
---
+++ appE4BloodBank/Presentacion/frmLogin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace appE4BloodBank.Presentacion
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnINICIAR_Click(object sender, EventArgs e)
+        {
+            if tbUsuario.Text = "julian" and tbContraseña.text = "1234" Them 
+                
+        }
+    }
+}
